03ea04c853b1129f28b1c87bc7e678c8a99ae518
MonsterLlama <MonsterLlama@gmail.com>
2018-05-10 21:15:09 -0700

    Added LeetCode project and first problem/tests.

---
+++ LeetCode/LeetCode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterLlama.Studying.LeetCode
+{
+    public static partial class LeetCode
+    {
+
+        // https://leetcode.com/problems/two-sum/description/
+        //
+        //  Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+        //
+        //  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        //
+        public static (int indexA, int indexB) TwoSum(int[] A, int target)
+        {
+            var result = (-1, -1);
+
+            if (A == null || A.Length < 2)
+            {
+                return result;  // Should probably throw an Exception here ... ^ ^
+            }
+
+            // Dictionary's Keys will represent the values in the passsed in Array.
+            // Its Values will represent the Array index where that value resides.
+            //
+            // Add value/index for the first element of the Array.
+            var dict = new Dictionary<int, int>()
+            {
+                [A[0]] = 0
+            };
+
+            // Iterate over the rest of the passed in Array and check whether we
+            // have a match for the target when summing the values from two indexes.
+            for (int index = 0; index < A.Length; index++)
+            {
+                var match = target - A[index];
+
+                // Do we have a match for the current index?
+                if (dict.ContainsKey(match))
+                {
+                    return (indexA: dict[match], indexB: index);
+                }
+                else
+                {
+                    dict[A[index]] = index;
+                }
+            }
+
+
+            return result;
+        }
+
+    }
+}
---
+++ LeetCodeTestHarness/LeetCodeTesting.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Diagnostics.Debug;
+
+
+namespace MonsterLlama.Studying.LeetCode
+{
+    public class LeetCodeTesting
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 3, 5, 7, 8, 9 };
+
+            var result = LeetCode.TwoSum(array1, 13);
+                result = LeetCode.TwoSum(array1, 16);
+                result = LeetCode.TwoSum(array1, 10);
+
+            WriteLine($"*** (Debug Info: {array1[result.indexA]} + {array1[result.indexB]} = 10) ***");
+
+                result = LeetCode.TwoSum(array1, 20);
+            
+        }
+    }
+}
